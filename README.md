ae711cd531896ab29b2bcdbf008dbf94da110d7e
한덕용 <128759865+HyperQuanx@users.noreply.github.com>
2023-11-17 16:45:53 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+yarn add
+react-router-dom
+react-redux @reduxjs/toolkit
+redux react-redux
+styled-components
+axios
+redux-thunk
+react-query
---
+++ src/shared/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+    <Router>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+    </Router>
+    </BrowserRouter>
+  )
+}
+
+export default Router
